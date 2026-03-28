851894973f8afaaa3b45f5ee640e774e29ce0143
Pkriegerjr <143087551+pkriegerjr@users.noreply.github.com>
2026-03-27 23:10:19 -0300

    Add files via upload
    falta criar as interações com javascript para digitar informações necessarias para o trabalho

---
+++ aula3.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHTMLJAVA</title>
+        <style>
+            body {
+                
+            }
+            .centro{
+                text-align: center;
+
+            }
+
+        </style>
+    </head>
+    <body>
+        <div class="centro"><h1>Digite suas informações pessoais</h1></div>
+
+        
+
+        <script>
+
+        </script>
+        <?php
+        $nomes = [];
+        $idade = [];
+        $Ucartao= [];
+        ?>
+    </body>
+</html>
