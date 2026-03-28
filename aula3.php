a8e3ae32a1438f878b22bb81ddfdaee9844fa328
Pkriegerjr <143087551+pkriegerjr@users.noreply.github.com>
2026-03-28 12:21:03 -0300

    Update aula3.php

--- aula3.php
+++ aula3.php
@@ -1,20 +1,20 @@
 <?php
-session_start(); // Faz o PHP "lembrar" de quem está acessando
+session_start(); // Faz o PHP "lembrar" das infos
 
-// 1. Pega os dados do JavaScript
+// Pega os dados do JavaScript
 $json = file_get_contents("php://input");
 $dadosF = json_decode($json, true);
 
 if ($dadosF) {
-    // 2. Se a lista de filmes ainda não existe na "memória", cria ela
+    // Se a lista de filmes ainda não existe na "memória", cria ela
     if (!isset($_SESSION['meusFilmes'])) {
         $_SESSION['meusFilmes'] = [];
     }
 
-    // 3. Adiciona o filme atual à lista da sessão
+    // Adiciona o filme atual no array
     $_SESSION['meusFilmes'][] = $dadosF;
 
-    // 4. Responde para o JavaScript (Acessando a chave 'titulo')
+    // Responde para o JavaScript (Acessando a chave 'titulo')
     echo "Sucesso! O filme " . $dadosF['filme'] . " foi guardado no servidor.";
 } else {
     echo "Erro: Nenhum dado recebido.";
